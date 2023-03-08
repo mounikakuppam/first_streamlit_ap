7d9446e6561fdcee83eefc8ca6781ba5d9665639
mounikakuppam <127034076+mounikakuppam@users.noreply.github.com>
2023-03-08 11:08:19 +0530

    Update streamlit_ap.py
    added breakfast menu and three items

--- streamlit_ap.py
+++ streamlit_ap.py
@@ -1,6 +1,7 @@
 import streamlit
 streamlit.title('My parents new healthy dinner')
 streamlit.header('Breakfast Menu')
-streamlit.text('omega 3 & blueberry')
-streamlit.text('idli & puri')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
 
